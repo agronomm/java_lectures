777f5b66a8cd13e3a481ace526a0891cf0a4921f
AvtoSE <OlegGUA@gmail.com>
2012-10-04 23:41:19 +0300

    Chto-to ne tak

---
+++ oleg/src/Array_Les1.java
@@ -0,0 +1,13 @@
+public class Array_Les1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] probam = new int[10];
+		for (int probai = 1; probai < 11; probai++) {
+			probam[probai] = probai;
+		}
+	}
+}
