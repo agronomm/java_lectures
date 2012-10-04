191047879040ddb293b27831f2573d280ce0f17b
AvtoSE <OlegGUA@gmail.com>
2012-10-04 23:57:56 +0300

    Problem 2

--- oleg/src/Array_Les1.java
+++ oleg/src/Array_Les1.java
@@ -6,8 +6,8 @@ public class Array_Les1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[] probam = new int[10];
-		for (int probai = 1; probai < 11; probai++) {
-			probam[probai] = probai;
+		for (int probai = 0; probai < 10; probai++) {
+			probam[probai] = probai + 1;
 		}
 	}
 }
