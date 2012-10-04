c58d45abe80a0a024e764c6be30dedd4b657f66a
KOT <cezap22@yandex.ru>
2012-10-05 00:21:05 +0400

    mass from 1 to 10

---
+++ kot/src/Array.java
@@ -0,0 +1,19 @@
+
+public class Array {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] masive;
+		masive = new int[10];
+		for (int con = 0; con < 10; con++) {
+			masive[con] =con +1;
+			System.out.println("номер= " + masive[con]);
+		}
+		
+	}
+	
+
+}
