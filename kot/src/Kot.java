347d7c6a4f07ad0435747364fdc5a746002c5281
KOT <cezap22@yandex.ru>
2012-10-04 23:48:30 +0400

    file

---
+++ kot/src/Kot.java
@@ -0,0 +1,12 @@
+
+
+public class Kot {
+public static void main(String [] args) {
+	String x = "Kot";
+	int y = 2;
+	String sum = y + x;
+	System.out.println(x);
+	System.out.println(y);
+	System.out.println(sum);
+}
+}
