6d6bb928fd794425c1af498db2a654fc409a7437
agronomm <agronomm@gmail.com>
2012-10-04 22:18:54 +0300

    Add test file

--- agronomm/lesson04/DoWhileCycle.java
+++ agronomm/lesson04/DoWhileCycle.java
@@ -2,6 +2,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+
+
 public class DoWhileCycle {
 	public static void main(String[] args) throws IOException {
 		double inputVar;
