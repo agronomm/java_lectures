03e08b109cb480c291d865fce8b7beb9f461265b
boitcex <boitcex@mail.ru>
2012-10-09 22:53:45 +0300

    проверь

--- roman/src/Array1.java
+++ roman/src/Array1.java
@@ -4,9 +4,9 @@ public class Array1 {
 		namber = new int[45];
 		for (int counter = 0; counter < 101; counter++) {
 			namber[counter] = 10 + counter * 2;
+		}
 			for (int i = 0; i < namber.length; i++) {
 				System.out.println(namber[i]);
 			}
 		}
 	}
-}
