3b8887200f2f536e22976046801ddb0b5476d0e3
agronomm <agronomm@gmail.com>
2012-10-09 23:40:28 +0300

    Five lesson

---
+++ agronomm/lesson05/FirstIfTest.java
@@ -0,0 +1,39 @@
+
+public class FirstIfTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int [] array = new int [10];
+		for (int i = 0, j = 1; i < 10; i++, j++) {
+			array[i] = j;
+		}
+		
+		int summ = 0;
+		
+		
+		
+		
+		
+		for (int i = 0; i < array.length; i++) {
+			if (i != 1 && i != 3) {
+				summ += array[i];
+			}
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		System.out.println("summ = " + summ);
+		
+
+	}
+
+}
